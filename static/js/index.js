fe5846163e53776aa60b48cd9bfe3a68cd7ecc61
kimulu <kimulumichael@gmail.com>
2023-03-04 10:10:02 +0300

    make cart work

--- static/js/index.js
+++ static/js/index.js
@@ -9,6 +9,17 @@ fetch('/get_cart_items_count') // Send a GET request to the server to get the ca
 });
 }
 
+function updateCartIcon2() {
+  var cartCount = document.getElementById('cart-count2');
+  var itemCount = 0;
+  fetch('/get_cart_items_count') // Send a GET request to the server to get the cart items count
+  .then(response => response.json()) // Parse the JSON response from the server
+  .then(data => {
+    itemCount = data.count; // Update the item count with the response data
+    cartCount.innerText = itemCount; // Update the cart icon
+  });
+  }
+
 $(document).ready(function() {
   $(".alert").fadeTo(2000, 500).slideUp(1000, function(){
     $(".alert").slideUp(500);
